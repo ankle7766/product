ec42e58be27af473e93d15b44662924fe9e8669a
Ankle Pan <pan7766118@gmail.com>
2021-02-04 17:24:46 +0800

    check file

--- product.py
+++ product.py
@@ -1,14 +1,21 @@
-# 讀取檔案
+import os   #operation system
+
 
 products = []
-with open('product.csv','r',encoding = 'utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue   #下面不執行，換下一回
-        name, price = line.strip().split(',')
-        products.append([name,price])
+#檢查檔案室存在
+if os.path.isfile('product.csv'):
+    print('found it !!!')
+    # 讀取檔案
+    with open('product.csv','r',encoding = 'utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue   #下面不執行，換下一回
+            name, price = line.strip().split(',')
+            products.append([name,price])
 
-print(products)
+    print(products)
+else:
+    print('cant found it.... ')
 
 while True:
     name = input("請輸入商品名字:")
